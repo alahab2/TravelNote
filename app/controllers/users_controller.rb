40fb334d9b99a4283b83dc1367fdfec1004c00bd
alahab <maria.dku.kz@gmail.com>
2016-08-30 21:08:05 +0800

    Everything is working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @user_notes = @user.notes
+    @user_notes = @user.notes.order('updated_at DESC')
   end
 end
