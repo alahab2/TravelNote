d779eb6ca784761d8a97fb65ae04ddf13bc10c2e
alahab <maria.dku.kz@gmail.com>
2016-08-30 15:43:59 +0800

    Styled Jumbotron New note button

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,4 +15,8 @@ class ApplicationController < ActionController::Base
     # devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
   
+
+  def after_sign_in_path_for(resource)
+    user_path(current_user)
+  end
 end
