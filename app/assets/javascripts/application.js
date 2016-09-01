9cee7595a2bb54eac13eedc6c322f23362739e79
alahab <maria.dku.kz@gmail.com>
2016-09-02 00:26:28 +0800

    Bootstrap poppers for private notes are working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -29,4 +29,8 @@ $(window).load(function(){
     itemSelector: '.grid-item',
     layoutMode: 'masonry'
   });
+
+  $("[data-toggle='tooltip']").tooltip();
+  $("[data-toggle='popover']").popover({trigger: "click"});
 });
+
