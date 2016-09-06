bcd6ec79666e5b98d7de6c296219852cdc41d507
alahab <maria.dku.kz@gmail.com>
2016-09-06 13:46:44 +0800

    Static maps and autocomplete are working with the right API

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -6,7 +6,7 @@ Geocoder.configure(
   # use_https: false,           # use HTTPS for lookup requests? (if supported)
   # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
   # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
-  api_key: 'AIzaSyBcIrmtLwU8pOKy1aM6VLuLKnEviXY_k9w'               # API key for geocoding service
+  api_key: 'AIzaSyBtky2vEg3OiQ80-JwbD-zpbNna52fidk4'               # API key for geocoding service
   # cache: nil,                 # cache object (must respond to #[], #[]=, and #keys)
   # cache_prefix: 'geocoder:',  # prefix (string) to use for all cache keys
 
