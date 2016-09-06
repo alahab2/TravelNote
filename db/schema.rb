77c3c47e76d6c327f5293e1c1d3b94cf66bf5acf
alahab <maria.dku.kz@gmail.com>
2016-09-06 09:33:05 +0800

    All good with migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160901145839) do
+ActiveRecord::Schema.define(version: 20160906012250) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,8 @@ ActiveRecord::Schema.define(version: 20160901145839) do
     t.string   "image"
     t.integer  "user_id"
     t.boolean  "private",            default: false
+    t.float    "longitude"
+    t.float    "latitude"
   end
 
   create_table "users", force: :cascade do |t|
