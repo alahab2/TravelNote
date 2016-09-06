3a1c188ce96af0c4e7227dc766a2335f99125281
alahab <maria.dku.kz@gmail.com>
2016-09-06 15:10:47 +0800

    Added a country attribute to DB. Now it's being populated

--- app/models/note.rb
+++ app/models/note.rb
@@ -38,4 +38,10 @@ class Note < ActiveRecord::Base
   geocoded_by :location
   after_validation :geocode, if: ->(obj){ obj.location.present? and obj.location_changed? }
 
-end
+  reverse_geocoded_by :latitude, :longitude do |obj,results|
+    if geo = results.first
+      obj.country = geo.country
+    end
+  end
+  after_validation :reverse_geocode
+  end
