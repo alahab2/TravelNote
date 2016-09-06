8c3c4458f18b33939df9e885e17d776fe9fc276d
alahab <maria.dku.kz@gmail.com>
2016-09-06 23:49:10 +0800

    Changes migration

--- db/migrate/20160906012250_add_columns_to_notes.rb
+++ db/migrate/20160906012250_add_columns_to_notes.rb
@@ -1,8 +1,8 @@
 class AddColumnsToNotes < ActiveRecord::Migration
   def change
-    remove_column :notes, :longitude, :float
+    
     add_column :notes, :longitude, :float
-    remove_column :notes, :latitude, :float
+    
     add_column :notes, :latitude, :float
   end
 end
